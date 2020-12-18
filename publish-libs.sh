c18f84763620aa37028901406a58ec71953d1671
dhowe <dhowe@cims.nyu.edu>
2020-12-18 22:13:08 +0800

    adjustments

--- publish-libs.sh
+++ publish-libs.sh
@@ -2,104 +2,139 @@
 
 set -e
 
-DO_JAVA=true
-DO_JS=true
-RITA_JAVA=../RiTa2
-RITA_JS=../rita2js
-POM=$RITA_JAVA/pom.xml
-PKG=$RITA_JS/package.json
-TMP=/tmp
+start=`date +%s`
+
+# paths
+tmp="/tmp"
+rita_java=."./RiTa2"
+rita_js="../rita2js"
+pom="$rita_java/pom.xml"
+pkg="$rita_js/package.json"
+
+# options
+nojs=false
+nojava=false
+nopub=false
 
 check_err() {
     local exit_code=$1
     shift
-    [[ $exit_code ]] &&         
-    ((exit_code != 0)) && {
+    [[ $exit_code ]] && ((exit_code != 0)) && {
         printf '\nFATAL: %s\n' "$@" >&2
         exit "$exit_code"
     }
 }
 
-[ "$DO_JAVA" = false ] && [ "$DO_JS" = false ] && check_err 1 "DO_JAVA and DO_JS false" #change to skip
+[ "$nojs" = true ] && [ "$nojava" = true ] && check_err 1 "nothing to do"
 
-while getopts "v:" option; do
+while getopts "v:s" option; do
     case ${option} in
-        v) VERSION=$OPTARG
-            echo "\n... using version: $VERSION"
+        v) version=$OPTARG
+            echo "\n... using version: $version"
+        ;;
+        s) nopub=true
+            echo "... skipping publish step"
         ;;
     esac
 done
 
-if [ -z $VERSION ] ; then
-    pushd $RITA_JS >/dev/null
-    VERSION=`npx npe version`
+if [ -z $version ] ; then
     ./check-env.sh || check_err $? "env check failed"
+    pushd $rita_js >/dev/null
+    version=`npx npe version`
     popd >/dev/null
-else 
-    ./bump-vers.sh $VERSION || check_err $? "bump-vers.sh failed"
+else
+    ./bump-vers.sh $version || check_err $? "bump-vers.sh failed"
 fi
 
-[ -z $VERSION ] && check_err 1 "No version found or supplied"
+[ -z $version ] && check_err 1 "No version found or supplied"
 
-if [ "$DO_JS" = true ] ; then         # build.test JavaScript
+if [ "$nojs" = false ] ; then         # build.test JavaScript
+    
     echo "... building with yarn"
-    yarn --cwd  $RITA_JS build >/dev/null || check_err $? "yarn build failed"
+    yarn --cwd  $rita_js build >/dev/null || check_err $? "yarn build failed"
     
     echo "... testing with yarn"
-    yarn --cwd  $RITA_JS test.prod >/dev/null || check_err $? "yarn tests failed"
+    yarn --cwd  $rita_js test.prod >/dev/null || check_err $? "yarn tests failed"
     
-    echo "... packaging for npm"
-    rm -rf $RITA_JS/rita-*.tgz
-    pushd $RITA_JS >/dev/null
+    echo "... packaging with npm"
+    rm -rf $rita_js/rita-*.tgz
+    pushd $rita_js >/dev/null
     npm pack --quiet >/dev/null || check_err $? "npm pack failed"
     popd >/dev/null
 fi
 
-ARTIFACTS=./artifacts
-[[ -d $ARTIFACTS ]] || mkdir $ARTIFACTS
 
-if [ "$DO_JS" = true ] ; then
-    echo
-    read -p "publish v$VERSION to npm? " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]] ; then
-        echo "... git-tag js v$VERSION"
-        pushd $RITA_JS >/dev/null
-        git tag -a v$VERSION -m "Release v$VERSION"
-        git push -q origin --tags
-        NPM_TAR=rita-$VERSION.tgz
-        echo "... deploying to npm"
-        npm publish $NPM_TAR --quiet || check_err $? "npm publish failed"
-        popd >/dev/null
-        mv $RITA_JS/*.tgz  $ARTIFACTS
+artifacts=./artifacts
+echo "... cleaning $artifacts"
+[[ -d $artifacts ]] || mkdir $artifacts
+rm -f $artifacts/*.* >/dev/null 
+
+if [ "$nojs" = false ] ; then
+    
+    if [ "$nopub" = false ] ; then
+        echo
+        read -p "publish v$version to npm? " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]] ; then
+            echo "... git-tag js v$version"
+            pushd $rita_js >/dev/null
+            git tag -a v$version -m "Release v$version"
+            git push -q origin --tags
+            NPM_TAR=rita-$version.tgz
+            echo "... deploying to npm"
+            npm publish $NPM_TAR --quiet || check_err $? "npm publish failed"
+            popd >/dev/null
+            mv $rita_js/*.tgz  $artifacts
+        fi
     fi
-    compgen -G $ARTIFACTS/*.js && mv $ARTIFACTS/*.js $TMP # clean previous versions
-    compgen -G $ARTIFACTS/*.tgz && mv $ARTIFACTS/*.tgz $TMP 
-    compgen -G $RITA_JS/dist/*.js && cp $RITA_JS/dist/*.js $ARTIFACTS
-    compgen -G $RITA_JS/*.tgz && mv $RITA_JS/*.tgz $ARTIFACTS
+    
+    compgen -G $artifacts/*.js >/dev/null && mv $artifacts/*.js $tmp  # clean previous versions
+    compgen -G $artifacts/*.tgz >/dev/null && mv $artifacts/*.tgz $tmp
+    compgen -G $rita_js/dist/*.js >/dev/null && cp $rita_js/dist/*.js $artifacts
+    compgen -G $rita_js/*.tgz >/dev/null && mv $rita_js/*.tgz $artifacts
 fi
 
-if [ "$DO_JAVA" = true ] ; then
-    echo
-    read -p "publish java v$VERSION to github? " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]] ; then
-        pushd $RITA_JAVA >/dev/null
-        echo "... git-tag java v$VERSION"
-        git tag -a v$VERSION -m "Release v$VERSION"
-        git push -q origin --tags
-        echo "... deploying to github packages"
-        mvn -q clean deploy || check_err $? "maven publish failed"
+if [ "$nojava" = false ] ; then
+    if [ "$nopub" = false ] ; then
+        echo
+        read -p "publish java v$version to github? " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]] ; then
+            pushd $rita_java >/dev/null
+            echo "... git-tag java v$version"
+            git tag -a v$version -m "Release v$version"
+            git push -q origin --tags
+            echo "... deploying to github packages"
+            mvn -q -T1C clean deploy || check_err $? "maven publish failed"
+            popd >/dev/null
+        fi
+    else
+        pushd $rita_java >/dev/null
+        echo "... creating maven packages"
+        mvn -q -T1C clean package || check_err $? "maven build failed"
         popd >/dev/null
-        # remove previous versions
-        rm -rf $ARTIFACTS/rita-*.jar $ARTIFACTS/rita-*.pom $ARTIFACTS/rita-*.asc 2>/dev/null 
-        compgen -G $RITA_JAVA/target/rita-$VERSION* && cp $RITA_JAVA/target/rita-$VERSION* $ARTIFACTS 
     fi
+    # remove previous versions
+    rm -rf $artifacts/rita-*.jar $artifacts/rita-*.pom $artifacts/rita-*.asc 2>/dev/null
+    compgen -G $rita_java/target/rita-$version* >/dev/null && cp \
+    $rita_java/target/rita-$version* $artifacts
 fi
+
+echo "... creating zip"
+zipfile=rita-$version-all.zip
+rm -f rita-$version-all.zip 2>/dev/null 
+pushd $artifacts >/dev/null
+zip ../$zipfile ./* >/dev/null
+popd >/dev/null
 
 
 echo "... cleaning up"
-#rm -rf $RITA_JS/*.tgz
+#rm -rf $rita_js/*.tgz
 
-echo "... finished\n"
-ls -l $ARTIFACTS
+runtime=$((`date +%s`-start))
+
+echo "... done in ${runtime}s\n"
+
+ls -l $artifacts
+echo; jar tf $zipfile
