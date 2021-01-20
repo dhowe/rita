a4064daa6f6aa3bf3b9b9e4f65882aeae6e8a84a
dhowe <dhowe@cims.nyu.edu>
2021-01-20 14:40:14 +0800

    tweak to docs,script

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+tmp="/tmp"
 dest="./pub"
 ritajs="../ritajs"
+
+# move aside rita.txt and rita.zip if we have them
+compgen -G $dest/rita.txt >/dev/null && mv $dest/rita.txt $tmp
+compgen -G $dest/rita.zip >/dev/null && mv $dest/rita.zip $tmp
 
 # clean
 rm -rf $dest
 mkdir $dest && mkdir $dest/dist
+
+# move aside rita.txt and rita.zip if we have them
+compgen -G $tmp/rita.txt >/dev/null && mv $tmp/rita.txt $dest
+compgen -G $tmp/rita.zip >/dev/null && mv $tmp/rita.zip $dest
 
 # copy ws
 echo ... making website 
