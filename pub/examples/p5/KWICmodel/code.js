c2f973716f80c633ba85be45100bbf4bac560f1b
dhowe <dhowe@cims.nyu.edu>
2022-05-16 13:05:22 +0800

    update examples

--- pub/examples/p5/KWICmodel/code.js
+++ pub/examples/p5/KWICmodel/code.js
@@ -1,3 +1,4 @@
+
 let txt, word, keywords = [
   "Gregor", "Samsa", "family", "being",
   "clerk", "room", "violin", "window"
@@ -13,34 +14,39 @@ function setup() {
   createCanvas(800, 500);
   textFont('Times New Roman', 16);
 
-  RiTa.concordance(txt.join('\n'));
-  word = RiTa.random(keywords);
+  RiTa.concordance(txt.join('\n')); // create concordance
+  word = RiTa.random(keywords); // pick a word to start
 
   createButtons();
 }
 
 function draw() {
 
+  // get kwic lines for the word
   let kwic = RiTa.kwic(word, 6);
   let tw = textWidth(word) / 2;
 
   background(250);
-  for (let i = 0; i < kwic.length; i++) {
+  let num = min(10, kwic.length);
+  for (let i = 0; i < num; i++) {
 
-    let parts = kwic[i].split(word);
-    let x = width / 2, y = i * 20 + 75;
+    // get the parts, stripping line-breaks
+    let parts = kwic[i].split(word)
+      .map(p => p.replace(/\n/g,' '));
+    
+    let x = width / 2, y = i * 24 + 90;
     if (y > height - 20) return;
 
     fill(0);
-    textAlign(RIGHT);
+    textAlign(RIGHT); // context-before-word
     text(parts[0], x - tw, y);
 
     fill(200, 0, 0);
     textAlign(CENTER);
-    text(word, x, y);
+    text(word, x, y);  // the word itself
 
     fill(0);
-    textAlign(LEFT);
+    textAlign(LEFT); // context-after-word
     text(parts[1], x + tw, y);
   }
   noLoop();
@@ -59,19 +65,24 @@ function createButtons() {
     buttons.push(button);
   }
 
-  // center and set the position for each
-  let totalW = (keywords.length - 1 * gap) + buttonsW;
+  // center and position each button
+  let totalW = (keywords.length * gap) + buttonsW;
   let sofar = 0, startX = width / 2 - totalW / 2;
   for (let i = 0; i < buttons.length; i++) {
-    buttons[i].position(startX + sofar, 65);
+    buttons[i].position(startX + sofar, 30);
     sofar += buttons[i].width + gap;
   }
 
-  // add handler function to each
+  // add handler function for each
   buttons.forEach((b, i) => b.mouseClicked(() => {
     word = b.elt.textContent;
-    buttons.forEach(b => b.style('color', 'black'));
-    b.style('color', 'rgb(200,0,0)');
+    buttons.forEach(but => but.style('color', 'black'));
+    b.style('color', 'rgb(200,0,0)'); // make clicked button red
     loop();
   }));
 }
+
+    
+
+
+
