840b76b764c5e7dc80cce7273dcef254f38b5d92
dhowe <daniel@rednoise.org>
2025-06-10 13:31:39 -0400

    update index

--- scripts/watch-site.sh
+++ scripts/watch-site.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# NOTE: requires entr to be installed
+# Usage: ./scripts/watch-site.sh [additional arguments for build-site.sh]
+
 find ./docgen ./www  -name '*' | entr ./scripts/build-site.sh $@
